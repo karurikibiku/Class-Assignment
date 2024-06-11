a1c3dc9cb9020a36c5b5613d97be930b58c9dc6d
karurikibiku <samuel.karuri@strathmore.edu>
2024-06-11 20:11:38 +0300

    Add files via upload

---
+++ Class assignment III.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "     O      O\n";
+    cout << "  >--|--<>--|--<  /|_||_\\`.__\n";
+    cout << "     |     /_\\ (   _    _ _\\\\\n";
+    cout << "..../ \\.../   \\.`-(_)--(_)-'.....\n";
+}
